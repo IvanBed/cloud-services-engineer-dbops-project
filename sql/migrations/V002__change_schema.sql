28015d10b2508f36d29b97b4594b47e742e0ba21
ivan <ivanbedaev1@gmail.com>
2025-03-29 21:01:53 +0000

    Добавление первичных ключей в orders, product.

--- sql/migrations/V002__change_schema.sql
+++ sql/migrations/V002__change_schema.sql
@@ -1,6 +1,7 @@
 ALTER TABLE product ADD COLUMN price double precision;
-
+ALTER TABLE product ADD CONSTRAINT pk_product PRIMARY KEY (id);
 ALTER TABLE orders ADD COLUMN date_created date;
+ALTER TABLE orders ADD CONSTRAINT pk_orders PRIMARY KEY (id);
 
 ALTER TABLE order_product ADD CONSTRAINT fk_order
 FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
