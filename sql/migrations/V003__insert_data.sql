110482f7cdedb8477f3cdfabee12549cb9f45c9a
IvanBed <116827491+IvanBed@users.noreply.github.com>
2025-03-31 12:57:48 +0300

    (Fix) Изменение скрипта внесения данных, id заданы явно.

--- sql/migrations/V003__insert_data.sql
+++ sql/migrations/V003__insert_data.sql
@@ -1,14 +1,10 @@
+INSERT INTO product (id, name, picture_url, price) VALUES 
+(1, 'Сливочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/6.jpg', 320.00),
+(2, 'Особая', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/5.jpg', 179.00),
+(3, 'Молочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/4.jpg', 225.00),
+(4, 'Нюренбергская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/3.jpg', 315.00),
+(5, 'Мюнхенская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/2.jpg', 330.00),
+(6, 'Русская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/1.jpg', 189.00);
 
-ALTER SEQUENCE product_id_seq RESTART WITH 1;
-
-ALTER SEQUENCE orders_id_seq RESTART WITH 1;
-
-INSERT INTO product (name, picture_url, price) VALUES ('Сливочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/6.jpg', 320.00);
-INSERT INTO product (name, picture_url, price) VALUES ('Особая', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/5.jpg', 179.00);
-INSERT INTO product (name, picture_url, price) VALUES ('Молочная', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/4.jpg', 225.00);
-INSERT INTO product (name, picture_url, price) VALUES ('Нюренбергская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/3.jpg', 315.00);
-INSERT INTO product (name, picture_url, price) VALUES ('Мюнхенская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/2.jpg', 330.00);
-INSERT INTO product (name, picture_url, price) VALUES ('Русская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/1.jpg', 189.00);
-
-INSERT INTO orders (status, date_created) SELECT (array['pending', 'shipped', 'cancelled'])[floor(random() * 3 + 1)], DATE(NOW() - (random() * (NOW() + '90 days'::interval - NOW())));
-INSERT INTO order_product (quantity, order_id, product_id) SELECT floor(1+random()*50)::int, i, 1 + floor(random()*6)::int % 6 FROM generate_series(1, 10000000) s(i);
+INSERT INTO orders (id, status, date_created) SELECT i, (array['pending', 'shipped', 'cancelled'])[floor(random() * 3 + 1)], DATE(NOW() - (random() * (NOW()+'90 days' - NOW()))) FROM generate_series(1, 1000000) s(i);
+INSERT INTO order_product (quantity, order_id, product_id) SELECT floor(1+random()*50)::int, i, 1 + floor(random()*6)::int % 6 FROM generate_series(1, 1000000) s(i);
