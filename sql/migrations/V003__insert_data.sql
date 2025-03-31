53025909981b2c822002c40899ee6a997960443b
IvanBed <116827491+IvanBed@users.noreply.github.com>
2025-03-31 13:06:16 +0300

    (Fix) Добавление OVERRIDING SYSTEM VALUE №2

--- sql/migrations/V003__insert_data.sql
+++ sql/migrations/V003__insert_data.sql
@@ -8,5 +8,7 @@ VALUES
 (5, 'Мюнхенская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/2.jpg', 330.00),
 (6, 'Русская', 'https://res.cloudinary.com/sugrobov/image/upload/v1623323635/repos/sausages/1.jpg', 189.00);
 
-INSERT INTO orders (id, status, date_created) SELECT i, (array['pending', 'shipped', 'cancelled'])[floor(random() * 3 + 1)], DATE(NOW() - (random() * (NOW()+'90 days' - NOW()))) FROM generate_series(1, 1000000) s(i);
+INSERT INTO orders (id, status, date_created) 
+OVERRIDING SYSTEM VALUE 
+SELECT i, (array['pending', 'shipped', 'cancelled'])[floor(random() * 3 + 1)], DATE(NOW() - (random() * (NOW()+'90 days' - NOW()))) FROM generate_series(1, 1000000) s(i);
 INSERT INTO order_product (quantity, order_id, product_id) SELECT floor(1+random()*50)::int, i, 1 + floor(random()*6)::int % 6 FROM generate_series(1, 1000000) s(i);
